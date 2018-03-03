e37523c5421cddfaf9daca10af5e49c40ee9439d
Harrison <harrisonhopkins@gmail.com>
2018-03-03 15:37:32 -0800

    Multithreading
    Experimental! Added multithreading. I ran into what appeared to be a rate limit on 23andMe's end, so it's limited to 4 threads. My test put it at ~20 minutes for 100 relatives, which is a lot faster than the non-multithreaded one. Once I clean up and refactor more, this will become the main branch

--- 23andMe Common Relatives Scraper.py
+++ 23andMe Common Relatives Scraper.py
@@ -5,7 +5,39 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 import csv
+from multiprocessing.dummy import Pool as ThreadPool
+import atexit
+from time import time, strftime, localtime
+from datetime import timedelta
 
+def secondsToStr(elapsed=None):
+    if elapsed is None:
+        return strftime("%Y-%m-%d %H:%M:%S", localtime())
+    else:
+        return str(timedelta(seconds=elapsed))
+
+def log(s, elapsed=None):
+    line = "="*40
+    print(line)
+    print(secondsToStr(), '-', s)
+    if elapsed:
+        print("Elapsed time:", elapsed)
+    print(line)
+    print()
+
+def endlog():
+    end = time()
+    elapsed = end-start
+    log("End Program", secondsToStr(elapsed))
+
+def split(arr, size):
+    arrs = []
+    while len(arr) > size:
+        pice = arr[:size]
+        arrs.append(pice)
+        arr   = arr[size:]
+    arrs.append(arr)
+    return arrs
 
 def pull_data_from_23andme_row(row_soup):
     common_relative_soup = row_soup.find('td')
@@ -20,6 +52,78 @@ def pull_data_from_23andme_row(row_soup):
 
     return [common_relative_id, common_relative, your_pct, their_pct]
 
+def get_data_from_23andme(split_ids):
+    driver = webdriver.Firefox()
+    global output_table
+    for relative_id, relative_name in split_ids.items():
+        driver.get(base_url.format(relative_id))
+
+        # Check if we need to sign in'
+        if 'Sign in' in driver.title:
+            print('Signing in to 23andMe...')
+            email_input = driver.find_element_by_id('email')
+            email_input.send_keys(email)
+            password_input = driver.find_element_by_id('password')
+            password_input.send_keys(password)
+            password_input.submit()
+
+        try:
+            waitingtime = WebDriverWait(driver, 30).until(
+                ec.presence_of_element_located((By.CLASS_NAME, 'js-relatives-in-common-table')))
+        except TimeoutException as ex:  # Page didn't open in time/load our table, let's skip it
+            print('Timed out while opening {}, moving to next ID.'.format(base_url.format(relative_id)))
+            continue
+
+        print('Pulling data for {}...'.format(relative_name))
+        current_page_soup = BeautifulSoup(driver.page_source, 'html.parser')
+        try:
+            current_page = int(current_page_soup.find(class_ = 'current-page').get_text().strip())
+            current_page_soup.find_previous_sibling()
+
+            try:
+                max_page = int(current_page_soup.find(class_ = 'ellipses').find_next_sibling('span').get_text())
+                print('{} pages of data to pull...'.format(max_page))
+            except (NoSuchElementException, AttributeError):
+                print('Fewer than 5 pages, I hope this works!')
+                max_page = 5
+
+            while current_page <= max_page:
+                print('Pulling {} page {} of {}'.format(relative_name, current_page, max_page))
+                relatives_table = current_page_soup.find(class_ = 'js-relatives-in-common-table')
+                for row in relatives_table.find_all('tr'):
+                    if row.find('td') and row.has_attr('class') and 'loading-row' not in row.get('class'):
+                        curr_common_relative = [relative_id, relative_name, relative_relations[relative_id]]
+                        curr_common_relative.extend(pull_data_from_23andme_row(row))
+                        output_table.append(curr_common_relative)
+                else:
+                    # time to load the next page
+                    current_page = current_page + 1
+                    try:
+                        driver.find_element_by_xpath(
+                            "//span[contains(@class,'current-page')]/following-sibling::span").click()
+                        waitingtime = WebDriverWait(driver, 30).until(
+                            ec.presence_of_element_located((By.CLASS_NAME, 'js-relative-row')))
+                        current_page_soup = BeautifulSoup(driver.page_source, 'html.parser')
+                    except NoSuchElementException:
+                        # We're on the last page
+                        continue
+                    except TimeoutException as ex:
+                        print('Timed out loading page {} of {}, moving to next relative.'.format(current_page, max_page))
+                        continue
+
+
+        except NoSuchElementException as ex:
+            print('Error pulling data for {}, moving to next'.format(relative_name))
+            print('Error: {}'.format(str(ex)))
+            output_table.append([relative_id, relative_name, '!ERROR!', '!ERROR!', '!ERROR!', '!ERROR!'])
+
+    driver.close()
+    return 'Success!'
+
+
+start = time()
+atexit.register(endlog)
+log('Start program')
 
 print('For this to work properly, the script needs to know how to log into your account')
 email = input('Please enter your email address: ')
@@ -33,6 +137,7 @@ if input('Is this correct? (Y/n) ') != 'Y':
 print('Now, we need to know where your 23andMe DNA Relatives aggregate data is saved')
 print('This csv should be unchanged from the download')
 file_of_ids = input('Please enter the full path of your DNA Relatives aggregate data, WITHOUT quotation marks: ')
+
 relative_ids = {}
 relative_relations = {}
 
@@ -45,14 +150,25 @@ try:
                 relative_ids[row[8][59:]] = row[0]
                 relative_relations[row[8][59:]] = row[14]
 
-        f.close()
 except FileNotFoundError as ex:
     print('Unable to open your DNA Relatives data, please re-check the path and try again.')
     print('Error: {}'.format(str(ex)))
     exit()
 
+
 if input('File has {} unique IDs, continue? (Y/n) '.format(len(relative_ids))) != "Y":
     exit()
+
+# for arr in split(list(relative_ids.keys()), 2):
+#     print(arr)
+
+relative_ids_split = split(list(relative_ids.keys()), 10)
+relative_ids_split_dicts = []
+for rel in relative_ids_split:
+    relative_ids_split_dicts.append({rel_id: relative_ids[rel_id] for rel_id in rel})
+# relative_ids_split_dicts = [{rel: relative_ids[rel] for rel in relative_ids_split}]
+print(relative_ids_split_dicts)
+
 
 output_table = [['Relative ID',
                  'Relative Name',
@@ -65,78 +181,13 @@ output_table = [['Relative ID',
 
 base_url = r'https://you.23andme.com/url/tools:compare_match/?remote_id={}'
 
-driver = webdriver.Firefox()
-for relative_id, relative_name in relative_ids.items():
 
-    driver.get(base_url.format(relative_id))
-
-    # Check if we need to sign in'
-    if 'Sign in' in driver.title:
-        print('Signing in to 23andMe...')
-        email_input = driver.find_element_by_id('email')
-        email_input.send_keys(email)
-        password_input = driver.find_element_by_id('password')
-        password_input.send_keys(password)
-        password_input.submit()
-
-    try:
-        waitingtime = WebDriverWait(driver, 30).until(
-            ec.presence_of_element_located((By.CLASS_NAME, 'js-relatives-in-common-table')))
-    except TimeoutException as ex:  # Page didn't open in time/load our table, let's skip it
-        print('Timed out while opening {}, moving to next ID.'.format(base_url.format(relative_id)))
-        continue
-
-    print('Pulling data for {}...'.format(relative_name))
-
-    current_page_soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    try:
-        current_page = int(current_page_soup.find(class_ = 'current-page').get_text().strip())
-        current_page_soup.find_previous_sibling()
-
-        try:
-            max_page = int(current_page_soup.find(class_ = 'ellipses').find_next_sibling('span').get_text())
-            print('{} pages of data to pull...'.format(max_page))
-        except (NoSuchElementException, AttributeError):
-            print('Fewer than 5 pages, I hope this works!')
-            max_page = 5
-
-        while current_page <= max_page:
-            print('Pulling {} page {} of {}'.format(relative_name, current_page, max_page))
-            relatives_table = current_page_soup.find(class_ = 'js-relatives-in-common-table')
-            for row in relatives_table.find_all('tr'):
-                if row.find('td') and row.has_attr('class') and 'loading-row' not in row.get('class'):
-                    curr_common_relative = [relative_id, relative_name, relative_relations[relative_id]]
-                    curr_common_relative.extend(pull_data_from_23andme_row(row))
-                    output_table.append(curr_common_relative)
-            else:
-                # time to load the next page
-                current_page = current_page + 1
-                try:
-                    driver.find_element_by_xpath(
-                        "//span[contains(@class,'current-page')]/following-sibling::span").click()
-                    waitingtime = WebDriverWait(driver, 30).until(
-                        ec.presence_of_element_located((By.CLASS_NAME, 'js-relative-row')))
-                    current_page_soup = BeautifulSoup(driver.page_source, 'html.parser')
-                except NoSuchElementException:
-                    # We're on the last page
-                    continue
-                except TimeoutException as ex:
-                    print('Timed out loading page {} of {}, moving to next relative.'.format(current_page, max_page))
-                    continue
-
-
-    except NoSuchElementException as ex:
-        print('Error pulling data for {}, moving to next'.format(relative_name))
-        print('Error: {}'.format(str(ex)))
-        output_table.append([relative_id, relative_name, '!ERROR!', '!ERROR!', '!ERROR!', '!ERROR!'])
-
-# print(output_table)
-
-driver.close()
+pool = ThreadPool(4)
+results = pool.map(get_data_from_23andme, relative_ids_split_dicts)
+print(results)
 
 print('Saving data...')
-with open("Common Relatives per Relative.csv", 'w', newline = '') as f:
+with open("Common Relatives per Relative_from_multithreading_full data.csv", 'w', newline = '') as f:
     writer = csv.writer(f)
     writer.writerows(row for row in output_table if row)
 
